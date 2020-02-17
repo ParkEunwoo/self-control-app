6de4e897ffa52537816ba4c5f9cd0368924b0780
ParkEunwoo <dmsdn960@gmail.com>
2020-02-17 15:38:55 +0900

    구글 로그인 버튼

--- lib/screens/AuthPage.dart
+++ lib/screens/AuthPage.dart
@@ -1,7 +1,10 @@
 import "package:flutter/material.dart";
-
+import "package:flutter_svg/flutter_svg.dart";
 
 class AuthPage extends StatelessWidget {
+  final Widget svg = SvgPicture.asset(
+    'assets/icons8-google.svg',
+  );
 
   @override
   Widget build(BuildContext context) {
@@ -10,27 +13,22 @@ class AuthPage extends StatelessWidget {
         title: Text("AuthPage"),
       ),
       body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Card(
-              elevation: 2.0,
-              child: Column(
-                children: <Widget>[
-                  Text(
-                    'hello',
-                    style: Theme.of(context).textTheme.display1,
-                  ),
-                ],
-              ),
-            ),
-          ],
-        ),
-      ),
+          child: FlatButton(
+              onPressed: () {},
+              child: Card(
+                elevation: 2,
+                child: Row(
+                  children: <Widget>[
+                    SizedBox(width: 20),
+                    new SvgPicture.asset('assets/icons8-google.svg'),
+                    SizedBox(width: 20),
+                    Text("구글로 로그인", style: TextStyle(fontSize: 24)),
+                  ],
+                ),
+              ))),
     );
   }
 }
-
 
 class AuthForm extends StatefulWidget {
   @override
@@ -38,10 +36,24 @@ class AuthForm extends StatefulWidget {
 }
 
 class _AuthFormState extends State<AuthForm> {
-
   final _formKey = GlobalKey<FormState>();
+  final titleController = TextEditingController();
+  final contentController = TextEditingController();
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Form(
+        key: _formKey,
+        child: Column(children: <Widget>[
+          TextFormField(
+            // The validator receives the text that the user has entered.
+            validator: (value) {
+              if (value.isEmpty) {
+                return 'Please enter some text';
+              }
+              return null;
+            },
+          )
+        ]));
   }
 }
